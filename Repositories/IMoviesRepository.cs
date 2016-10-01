08b859dc9cf56f409aaf7d867799ab0226049bf9
Geoffrey BRUNOIS <geoffreybrunois@MAC54.local>
2016-10-01 02:19:05 +0200

    Step-3: add MoviesRepository

---
+++ Repositories/IMoviesRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using Web.MoviesApi.Models;
+
+namespace Web.MoviesApi.Repositories
+{
+    /// <summary>
+    /// Interface for movies repository
+    /// </summary>
+    public interface IMoviesRepository
+    {
+        /// <summary>
+        /// Fetch all movies
+        /// </summary>
+        /// <returns>All movies</returns>
+        Task<Movie[]> GetMovies();
+
+        /// <summary>
+        /// Fetch a movie from id
+        /// </summary>
+        /// <param name="id">Movie unique identifier</param>
+        /// <returns>A movie or null</returns>
+        Task<Movie> GetMovie(string id);
+
+        /// <summary>
+        /// Insert a new movie
+        /// </summary>
+        /// <param name="movie">A movie to insert</param>
+        /// <returns></returns>
+        Task InsertMovie(Movie movie);
+
+        /// <summary>
+        /// Delete an existing movie
+        /// </summary>
+        /// <param name="id">Movie unique identifier</param>
+        /// <returns></returns>
+        Task DeleteMovie(string id);
+
+        /// <summary>
+        /// Update a movie
+        /// </summary>
+        /// <param name="movie">A movie to update</param>
+        /// <returns></returns>
+        Task UpdateMovie(Movie movie);
+    }
+}
--- Tests/Controllers/MoviesControllerTests.cs
+++ Tests/Controllers/MoviesControllerTests.cs
@@ -1,52 +1,137 @@
-using System.IO;
-using System.Net.Http;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Web.MoviesApi.Controllers;
+using Web.MoviesApi.Models;
+using Web.MoviesApi.Repositories;
 using Xunit;
 
 namespace Web.MoviesApi.Tests.Controllers
 {
     public class MoviesControllerTests
     {
-        private readonly TestServer _server;
-        private readonly HttpClient _client;
-
         public MoviesControllerTests()
         {
-            var contentRoot = Directory.GetCurrentDirectory();
-            _server = new TestServer(new WebHostBuilder()
-                .UseContentRoot(contentRoot)
-                .UseStartup<Startup>());
-            _client = _server.CreateClient();
-        }
 
-        private async Task<string> GetResponseString(string request)
-        {
-            var response = await _client.GetAsync(request);
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
         }
 
         [Fact]
-        public async Task GetAllMoviesRequestShouldReturnAllMovies()
+        public void GetMethodShouldReturnAllMovies()
         {
-            // Act
-            var responseString = await GetResponseString("/server/api/movies");
-
-            // Assert
-            Assert.Equal("[{\"id\":\"1\",\"title\":\"Titre 1\"},{\"id\":\"2\",\"title\":\"Titre 2\"},{\"id\":\"3\",\"title\":\"Titre 3\"}]", responseString);
+            var movies = CreateMoviesCollection();
+            using (MoviesController movieController = new MoviesController(CreateMoviesRepositoryMock(movies)))
+            {
+                var result = movieController.Get();
+                Assert.Equal(movies.Length, result.Result.Count());
+            }
         }
 
         [Fact]
-        public async Task GetMovieRequestShouldReturnMovie()
+        public void GetMethodWithIdArgShouldReturnMovieIdentifiedById()
         {
-            // Act
-            var responseString = await GetResponseString("/server/api/movies/1");
+            var movies = CreateMoviesCollection();
+            using (MoviesController movieController = new MoviesController(CreateMoviesRepositoryMock(movies)))
+            {
+                var movie = movies[0];
+                var result = movieController.Get(movie.Id.ToString()).Result as OkObjectResult;
+                Assert.NotNull(result);
+                Assert.Equal(movie, result.Value);
+            }
+        }
 
-            // Assert
-            Assert.Equal("{\"id\":\"1\",\"title\":\"Titre 1\"}", responseString);
+        [Fact]
+        public void GetMethodWithUnknownIdArgShouldReturnNotFoundException()
+        {
+            var movies = CreateMoviesCollection();
+            using (MoviesController movieController = new MoviesController(CreateMoviesRepositoryMock(movies)))
+            {
+                var result = movieController.Get(Guid.NewGuid().ToString()).Result;
+                Assert.IsType<NotFoundResult>(result);
+            }
+        }
+
+        [Fact]
+        public void PostMethodShouldInsertNewMovie()
+        {
+            var movies = CreateMoviesCollection();
+            using (MoviesController movieController = new MoviesController(CreateMoviesRepositoryMock(movies)))
+            {
+                Movie newMovie = new Movie() { Title = "Titre 3" };
+                var result = movieController.Post(newMovie).Result;
+                Assert.IsType<OkObjectResult>(result);
+                var insertedMovie = ((OkObjectResult)result).Value as Movie;
+                Assert.NotNull(insertedMovie.Id);
+                Assert.NotEqual(string.Empty, insertedMovie.Id);
+            }
+        }
+
+        [Fact]
+        public void PutMethodShouldUpdateExitingMovie()
+        {
+            var movies = CreateMoviesCollection();
+            var moviesRepository = CreateMoviesRepositoryMock(movies);
+            using (MoviesController movieController = new MoviesController(moviesRepository))
+            {
+                Movie movie = new Movie() { Id = "1", Title = "Nouveau titre" };
+
+                var result = movieController.Put(movie).Result;
+                Assert.IsType<OkResult>(result);
+
+                moviesRepository.Received().UpdateMovie(NSubstitute.Arg.Any<Movie>());
+            }
+        }
+
+        [Fact]
+        public void PutMethodWhenMovieDontExistsShouldReturn304HttpError()
+        {
+            var movies = CreateMoviesCollection();
+            var moviesRepository = CreateMoviesRepositoryMock(movies);
+            using (MoviesController movieController = new MoviesController(moviesRepository))
+            {
+                Movie movie = new Movie() { Id = "3", Title = "Nouveau titre" };
+
+                var result = movieController.Put(movie).Result;
+                Assert.IsType<StatusCodeResult>(result);
+                Assert.Equal(((StatusCodeResult)result).StatusCode, 304);
+            }
+        }
+
+        private Movie[] CreateMoviesCollection()
+        {
+            return new Movie[] {
+                new Movie() {
+                    Id = "1",
+                    Title = "Titre 1"
+                },
+                new Movie() {
+                    Id = "2",
+                    Title = "Titre 2"
+                }
+                };
+        }
+
+        private IMoviesRepository CreateMoviesRepositoryMock(Movie[] collection)
+        {
+            var substitute = Substitute.For<IMoviesRepository>();
+
+            substitute.GetMovies().Returns(Task.FromResult(collection));
+
+            substitute.GetMovie(NSubstitute.Arg.Any<string>()).ReturnsForAnyArgs(arg1 => Task.FromResult(collection.Where(movie => movie.Id == arg1.Arg<string>()).FirstOrDefault()));
+
+            substitute.InsertMovie(NSubstitute.Arg.Any<Movie>()).ReturnsForAnyArgs(arg1 =>
+            {
+                var movie = arg1.Arg<Movie>();
+                movie.Id = "3";
+                return Task.FromResult(movie);
+            });
+            substitute.UpdateMovie(NSubstitute.Arg.Any<Movie>()).ReturnsForAnyArgs(arg1 =>
+           {
+               return Task.CompletedTask;
+           }
+            );
+            return substitute;
         }
     }
 }
