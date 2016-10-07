ac86f1afebc9dde4f39ac6cda249afcab7852813
GBrunois <brunois.g@sfeir.com>
2016-10-07 23:20:37 +0200

    Correction configure mongo-seed

--- mongo-seed/configure.js
+++ mongo-seed/configure.js
@@ -1,21 +1,11 @@
-use admin;
-db.createUser(
-    {
-        user: "siteUserAdmin",
-        pwd: "unPasswordQuiVaBien",
-        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
-    }
-);
+db = db.getSiblingDB('admin');
+print(db);
+db.createUser({ user: "siteUserAdmin", pwd: "unPasswordQuiVaBien", roles: [{ role: "userAdminAnyDatabase", db: "admin" }] });
 db.auth("siteUserAdmin", "unPasswordQuiVaBien");
-db.adminCommand({authSchemaUpgrade: 1});
+db.adminCommand({ authSchemaUpgrade: 1 });
 
-use angularmovies;
-db.createUser(
-    {
-        user: "angularUser",
-        pwd: "password",
-        roles: ["dbOwner"]
-    }
-);
+db = db.getSiblingDB('angularmovies');
+print(db);
+db.createUser({ user: "angularUser", pwd: "password", roles: ["dbOwner"] });
 db.auth("angularUser", "password");
 db.createCollection("movies");
