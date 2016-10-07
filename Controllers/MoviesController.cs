9675efcf5a941a6accc6f070e328f0159dcf66e4
Geoffrey BRUNOIS <geoffrey.brunois.externe@swisslife.fr>
2016-10-07 15:44:07 +0200

    Step-4 Solution add BadRequest test

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -39,6 +39,10 @@ namespace Web.MoviesApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Post([FromBody]Movie value)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             await _moviesDAO.InsertMovie(value);
             return Ok(value);
         }
@@ -47,6 +51,10 @@ namespace Web.MoviesApi.Controllers
         [HttpPut]
         public async Task<IActionResult> Put([FromBody]Movie value)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             Movie movie = await _moviesDAO.GetMovie(value.Id);
             if (movie != null)
             {
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.MoviesApi.Models
 {
@@ -11,6 +12,8 @@ namespace Web.MoviesApi.Models
 
         public string Id { get; set; }
 
+        [Required]
+        [StringLength(500, MinimumLength = 1)]
         public string Title { get; set; }
 
         public string Category { get; set; } 
@@ -23,12 +26,14 @@ namespace Web.MoviesApi.Models
 
         public string Actors { get; set; }
 
+        [StringLength(500)]
         public string Synopsis { get; set; }
 
         public int Rate { get; set; }
 
         public DateTime LastViewDate { get; set; }
 
+        [Range(1d,100d)]
         public decimal? Price { get; set; }
         
     }
